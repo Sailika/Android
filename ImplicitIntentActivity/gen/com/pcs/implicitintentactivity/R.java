8ad0bb66a33926859e2bdb40184a8f340790211d
HarishKumar93 <harishkumar.vanka@paradigmcreatives.com>
2014-09-10 12:07:02 +0530

    Implicit Activity has Created

--- ImplicitIntentActivity/gen/com/pcs/implicitintentactivity/R.java
+++ ImplicitIntentActivity/gen/com/pcs/implicitintentactivity/R.java
@@ -5,7 +5,7 @@
  * should not be modified by hand.
  */
 
-package com.pcs.parcelabledataactivity;
+package com.pcs.implicitintentactivity;
 
 public final class R {
     public static final class attr {
@@ -13,30 +13,8 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
-    public static final class id {
-        public static final int btn=0x7f060000;
-        public static final int print=0x7f060001;
-    }
-    public static final class layout {
-        public static final int parcelable=0x7f030000;
-        public static final int reciever=0x7f030001;
-    }
     public static final class string {
-        public static final int and=0x7f04000e;
-        public static final int app_name=0x7f040000;
-        public static final int button=0x7f040002;
-        public static final int eigth=0x7f04000a;
-        public static final int eleventh=0x7f04000d;
-        public static final int fifth=0x7f040007;
-        public static final int first=0x7f040003;
-        public static final int fourth=0x7f040006;
-        public static final int heading=0x7f040001;
-        public static final int ninth=0x7f04000b;
-        public static final int second=0x7f040004;
-        public static final int seventh=0x7f040009;
-        public static final int sixth=0x7f040008;
-        public static final int tenth=0x7f04000c;
-        public static final int third=0x7f040005;
+        public static final int app_name=0x7f030000;
     }
     public static final class style {
         /** 
@@ -60,10 +38,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f040000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f040001;
     }
 }
