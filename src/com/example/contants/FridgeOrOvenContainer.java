e76f9011d941de35e168e0f07070516546ad0e3d
Sailika <sailika.ethalapaka@paradigmcreatives.com>
2014-11-03 17:22:23 +0530

    Layout Changed

---
+++ src/com/example/contants/FridgeOrOvenContainer.java
@@ -0,0 +1,8 @@
+package com.example.contants;
+
+public interface FridgeOrOvenContainer {
+	public static final String FRIDGE_OR_OVEN = "fridge_oven";
+	public static final String FRIDGE = "fridge";
+	public static final String OVEN = "oven";
+
+}
