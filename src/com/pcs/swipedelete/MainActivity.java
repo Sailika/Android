84c69e8310376a2663d119885784b5d9a79bfe4b
Sailika <sailika.ethalapaka@paradigmcreatives.com>
2014-11-04 12:15:39 +0530

    Created List

--- src/com/pcs/swipedelete/MainActivity.java
+++ src/com/pcs/swipedelete/MainActivity.java
@@ -1,16 +1,61 @@
 package com.pcs.swipedelete;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+
+import com.pcs.adapters.Adapter;
+import com.pcs.model.User;
 
 public class MainActivity extends Activity {
+	private ListView list;
+	private ArrayList<User> userlist;
+	private Adapter adapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		list = (ListView) findViewById(R.id.list);
+		userlist = new ArrayList<User>();
+
+		User users = new User();
+		users.setName("Skype");
+		users.setMsgContent("Skype LoginSucessfull");
+		userlist.add(users);
+
+		users = new User();
+		users.setName("Gmail");
+		users.setMsgContent("Welcome to Gmail!");
+		userlist.add(users);
+
+		users = new User();
+		users.setName("Whatsappp");
+		users.setMsgContent("Start Using Whatsapp");
+		userlist.add(users);
+		
+		users = new User();
+		users.setName("John");
+		users.setMsgContent("Hello,How are You??");
+		userlist.add(users);
+		
+		users = new User();
+		users.setName("BaseCamp");
+		users.setMsgContent("Daily recap for thursday,Nov 21");
+		userlist.add(users);
+		
+		users = new User();
+		users.setName("Hike");
+		users.setMsgContent("Get Free Recharge,Start USing Hike");
+		userlist.add(users);
+
+		adapter = new Adapter(MainActivity.this, userlist);
+		list.setAdapter(adapter);
 	}
 
 	@Override
@@ -31,4 +76,5 @@ public class MainActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+
 }
